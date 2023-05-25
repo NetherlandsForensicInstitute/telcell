041cef9318983384a9ae46594461f8c30561b707
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2023-05-25 11:49:22 +0200

    Add additional checks to test_is_colocated

--- tests/data/test_colocation.py
+++ tests/data/test_colocation.py
@@ -3,10 +3,10 @@ from telcell.data.parsers import parse_measurements_csv
 
 
 def test_is_colocated(testdata_path):
-    tracks = parse_measurements_csv(testdata_path)
-    track_a = tracks[0]
-    track_b = tracks[1]
-    track_c = tracks[2]
+    track_a, track_b, track_c = parse_measurements_csv(testdata_path)
 
+    assert is_colocated(track_a, track_a) is True
     assert is_colocated(track_a, track_b) is True
+    assert is_colocated(track_b, track_a) is True
     assert is_colocated(track_a, track_c) is False
+    assert is_colocated(track_c, track_a) is False
