f61de5c41e74428019f06dc95dcba18351cca383
YASTO <135600115+yasto-nfi@users.noreply.github.com>
2023-06-16 11:57:39 +0200

    First working script

--- tests/make_test_measurements.py
+++ tests/make_test_measurements.py
@@ -1,4 +1,4 @@
-"""Script to generate fake measurements for the telcell project."""
+"""Script to generate fake measurements-data for the telcell project."""
 
 import random
 import datetime
@@ -7,76 +7,81 @@ from datetime import timedelta, timezone, datetime
 import numpy as np
 import pandas as pd
 
-startloc = (52.0907, 5.1214)
+df_list = []
+
 startdate = datetime(2023, 5, 17, 12, 00, 00, tzinfo=timezone.utc)
 
-id = 0
-ids = []
-track = []
-sensor = []
-timestamp = []
-cell_identifier = []
-lon = []
-lat = []
-degrees = []
-idx = []
-
-date = startdate
 grid_length = 36
-start_idx = (round(grid_length/2), round(grid_length/2))
-
+# Create equally spaced WGS-84 coordinates within the range of the original data
 lon_range = np.linspace(start=3.60848669870189, stop=6.97261243364462, num=grid_length)
 lat_range = np.linspace(start=50.7885407786764, stop=53.2155513728185, num=grid_length)
 
+for name in ["Bas", "Tim", "Stijn", "Daan", "Koen", "Martijn", "Pieter", "Maarten", "Henk", "Hans"]:
+    track = []
+    sensor = []
+    timestamp = []
+    cell_identifier = []
+    lon = []
+    lat = []
+    degrees = []
+    lon_idxs = []
+    lat_idxs = []
+    date = startdate
+    lon_idx, lat_idx = round(grid_length / 2), round(grid_length / 2)  # Starting position (center)
 
-for d in range(5):
-    for i in range(20):
-        ids.append(id)
-        id += 1
+    for day in range(5):
+        for measurement in range(20):
+            track.append(name)
+            sensor.append(name + "1")
 
-        track.append("Bas")
-        sensor.append("Bas1")
+            date += timedelta(minutes=random.randint(1, 30))
+            timestamp.append(date.isoformat(" "))
 
-        date += timedelta(minutes=random.randint(1,30))
-        timestamp.append((date).isoformat(" "))
+            cell_identifier.append("123-4-5678-9012")
 
-        cell_identifier.append("00-458-54-415")
+            # Randomly choose between going Up/Down, Left/Right on the grid
+            dx = random.choice([0, 1, 2, -1, -2])
+            dy = random.choice([0, 1, 2, -1, -2])
+            # Movements cannot exceed the grid boundaries
+            lon_idx = np.clip(lon_idx + dx, 0, grid_length-1)
+            lat_idx = np.clip(lat_idx + dy, 0, grid_length-1)
 
-        dx = random.choice([0, 1, 2, -1, -2])
-        dy = random.choice([0, 1, 2, -1, -2])
-        start_idx = (np.clip(start_idx[0] + dx, 0, grid_length-1), np.clip(start_idx[1] + dy, 0, grid_length-1))
+            lon.append(lon_range[lon_idx])
+            lat.append(lat_range[lat_idx])
 
-        lon.append(lon_range[start_idx[0]])
-        lat.append(lat_range[start_idx[1]])
+            degrees.append(0)
 
-        degrees.append(0)
+            lon_idxs.append(lon_idx)
+            lat_idxs.append(lat_idx)
 
-        idx.append(start_idx)
+        date = startdate + timedelta(days=day+1, minutes=random.randint(0, 10))
 
-    date = startdate + timedelta(days=d+1, minutes=random.randint(0,10))
+    df1 = pd.DataFrame({
+        'track': track,
+        'sensor': sensor,
+        'timestamp': timestamp,
+        'celldb.wgs84.lon': lon,
+        'celldb.wgs84.lat': lat,
+        'celldb.azimuth_degrees': degrees,
+        'lon_index': lon_idxs,
+        'lat_index': lat_idxs})
 
+    # For the next sensor on the same track, we randomly add variations to the timestamp and location
+    df2 = df1.copy()
+    df2["sensor"] = name + "2"
 
-df1 = pd.DataFrame({
-    'id': ids,
-    'track': track,
-    'sensor': sensor,
-    'timestamp': timestamp,
-    'celldb.wgs84.lon': lon,
-    'celldb.wgs84.lat': lat,
-    'celldb.azimuth_degrees': degrees,
-    'index':idx})
+    df2["timestamp"] = df2["timestamp"].apply(lambda x: (datetime.strptime(x, "%Y-%m-%d %H:%M:%S%z") +
+                                                         timedelta(minutes=random.randint(0, 10))).isoformat(" "))
 
+    df2["lon_index"] = df2["lon_index"].apply(
+        lambda x: np.clip(x + random.choices([0, 1, -1], weights=[0.7, 0.15, 0.15])[0], 0, grid_length-1))
+    df2["lat_index"] = df2["lat_index"].apply(
+        lambda x: np.clip(x + random.choices([0, 1, -1], weights=[0.7, 0.15, 0.15])[0], 0, grid_length-1))
 
+    df2["celldb.wgs84.lon"] = df2["lon_index"].apply(lambda x: lon_range[x])
+    df2["celldb.wgs84.lat"] = df2["lat_index"].apply(lambda x: lat_range[x])
 
+    df_list.extend([df1, df2])
 
-#df.to_csv('testdata.csv', index=False)
-print(df1)
-
-df2 = df1.copy()
-
-df2["timestamp"] = df2["timestamp"].apply(lambda x: (datetime.strptime(x, "%Y-%m-%d %H:%M:%S%z") + timedelta(minutes=random.randint(0,10))).isoformat(" "))
-df2["index"]
-
-print(df2)
-
-"2023-05-17 12:17:00+00:00"
+final_df = pd.concat(df_list).reset_index(drop=True).drop(["lon_index", "lat_index"], axis=1)
+final_df.to_csv("test_measurements.csv", index_label="id")
