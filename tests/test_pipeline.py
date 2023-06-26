9051d74a6cc4311716d7cba4ba75d16e8c88bf66
Lars Killaars <27420806+Killaars@users.noreply.github.com>
2023-06-26 11:35:03 +0200

    test for run_pipeline

---
+++ tests/test_pipeline.py
@@ -0,0 +1,12 @@
+from telcell.crunchers.dummycruncher import dummy_cruncher
+from telcell.models.dummy import DummyModel
+from telcell.pipeline import run_pipeline
+
+def test_pipeline(test_data):
+    data = list(dummy_cruncher(test_data))
+
+    model = DummyModel()
+    lrs, y_true = run_pipeline(data, model)
+
+    assert lrs  ==[1.0, 1.0, 1.0, 1.0]
+    assert y_true == [True, True, False, False]
