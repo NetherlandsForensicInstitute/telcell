4ba6344569093dac0d458d4bf25fc36e634fa77c
Lars Killaars <27420806+Killaars@users.noreply.github.com>
2023-06-27 11:35:49 +0200

    more tests for simplemodel

--- tests/models/test_simplemodel.py
+++ tests/models/test_simplemodel.py
@@ -1,11 +1,14 @@
 from datetime import timedelta, datetime, timezone
 
+from telcell.crunchers.dummycruncher import dummy_cruncher
 from telcell.data.parsers import parse_measurements_csv
 from telcell.models.simplemodel import get_switches, filter_delay,\
-    make_pair_based_on_rarest_location_within_interval
+    make_pair_based_on_rarest_location_within_interval, \
+    MeasurementPairClassifier
+from telcell.pipeline import run_pipeline
 
 
-def test_simplemodel(testdata_3days_path):
+def test_simplemodel(test_data, testdata_3days_path, test_measurements_path):
     track_a, track_b, track_c = parse_measurements_csv(testdata_3days_path)
 
     paired_measurements = get_switches(track_a, track_b)
@@ -39,3 +42,21 @@ def test_simplemodel(testdata_3days_path):
             track_b,
             round_lon_lats=True)
     assert rarest_measurement_pair
+
+    # test MeasurementPairClassifier
+    model = MeasurementPairClassifier(
+        colocated_training_data=parse_measurements_csv(
+            test_measurements_path))
+    data = list(dummy_cruncher(test_data))
+    lrs, y_true = run_pipeline(data, model)
+
+    assert lrs == [1.0, 1.0, 1.0, 1.0]
+    assert y_true == [True, True, False, False]
+
+    # test select_colocated_pairs
+    assert len(model.colocated_training_pairs) == 419
+    assert model.colocated_training_pairs[0].is_colocated == True
+    assert model.colocated_training_pairs[212].distance == 20458.722810043124
+
+if __name__ == '__main__':
+    test_simplemodel()
