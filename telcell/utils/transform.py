44481c57b3d99500fd5089060c1fc12cece7921f
wb <20113294+wowtor@users.noreply.github.com>
2023-12-01 14:06:54 +0100

    .

--- telcell/utils/transform.py
+++ telcell/utils/transform.py
@@ -148,7 +148,8 @@ def get_pair_with_rarest_measurement_b(
             category count is the number of occurrences of the category from
             measurement_b in the track history that is provided.
     """
-    sorted_pairs = _sort_pairs_based_on_rarest_location(switches, history_track_b, categorize_measurement_for_rarity, max_delay)
+    sorted_pairs = _sort_pairs_based_on_rarest_location(switches, history_track_b, categorize_measurement_for_rarity,
+                                                        max_delay)
     assert len(sorted_pairs) > 0
     return sorted_pairs[0]
 
